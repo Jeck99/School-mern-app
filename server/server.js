bf4e04dd4e20b4298413f4079c11527694537f95
jeck99 <jeck99@gmail.com>
2022-03-03 07:39:32 +0200

    mongodb connection and students routing

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+require('./DB');
+const cors = require('cors');
+const studentRoute = require('./routes/student-route');
 const app = express();
-
+app.use(cors());
+app.use(express.json());
+app.use('/students', studentRoute);
 app.listen(process.env.PORT)
 app.get('/', (req, res) =>res.send('server online!'));
