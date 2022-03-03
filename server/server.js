4abf11151a95743174034049d0bccb27e85394cd
jeck99 <jeck99@gmail.com>
2022-03-03 08:26:28 +0200

    path install

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 require('./DB');
 const cors = require('cors');
+const path = require('path');
 const studentRoute = require('./routes/student-route');
 const app = express();
 app.use(cors());
